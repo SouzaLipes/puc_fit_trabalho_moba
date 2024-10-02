a4f86831bfb1c5cf60bb9d363a806ee2f7fc9c2b
MoraisPedroo <124322407+MoraisPedroo@users.noreply.github.com>
2024-10-01 23:50:48 -0300

    Add files via upload

---
+++ puc_fit/lib/loginpage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:puclifters/models/mycolors.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  MyAppColors.azulEscuro,
+                  MyAppColors.verdeClaro,
+                ],
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centraliza verticalmente
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height *
+                          0.2), // Adiciona espaço em cima para centralização
+                  Image.asset('assets/logo.png', height: 200),
+                  const SizedBox(height: 16),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Matricula',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Chave de acesso',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Entrar'),
+                  ),
+                  const SizedBox(height: 4),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Não possui chave de acesso? Obter'),
+                  ),
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height *
+                          0.2), // Adiciona espaço embaixo
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ puc_fit/lib/models/mycolors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyAppColors {
+  static const Color azulEscuro = Color(0xFF003366);
+  static const Color cinzaClaro = Color(0xFFCCCCCC);
+  static const Color verdeClaro = Color(0xFF66CC66);
+  static const Color branco = Color(0xFFF0F0F0);
+}
