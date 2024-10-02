b4574a8d4b2204a2601aa2720d2a8d9a37f5a9dd
MoraisPedroo <124322407+MoraisPedroo@users.noreply.github.com>
2024-10-02 00:06:46 -0300

    Update loginpage.dart

--- puc_fit/lib/loginpage.dart
+++ puc_fit/lib/loginpage.dart
@@ -31,12 +31,12 @@ class _LoginPageState extends State<LoginPage> {
               padding: const EdgeInsets.all(16),
               child: Column(
                 mainAxisAlignment:
-                    MainAxisAlignment.center, // Centraliza verticalmente
+                    MainAxisAlignment.center, 
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   SizedBox(
                       height: MediaQuery.of(context).size.height *
-                          0.2), // Adiciona espaço em cima para centralização
+                          0.2), 
                   Image.asset('assets/logo.png', height: 200),
                   const SizedBox(height: 16),
                   const TextField(
@@ -64,7 +64,7 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   SizedBox(
                       height: MediaQuery.of(context).size.height *
-                          0.2), // Adiciona espaço embaixo
+                          0.2),
                 ],
               ),
             ),
